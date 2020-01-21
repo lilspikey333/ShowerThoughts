cface5ebf4dbd730634360adbd32daa7ec0e7ed4
Jamrod <myfavoritenoisemaker@gmail.com>
2020-01-21 13:52:18 -0700

    pre-pull

--- frontend/src/components/Post/index.js
+++ frontend/src/components/Post/index.js
@@ -19,15 +19,15 @@ class Post extends Component {
         this.setState( prevState => ({thought: str}))
     }
 
-    postThought = () => {
-        this.props.post({ ...this.state })
-    }
+    // postThought = () => {
+    //     this.props.post({ ...this.state })
+    // }
 
     render() {
         <div className="flex-container-column new-post">
             <input ref={(input) => this.input = input} type="text" id="name-input" value={this.state.name} onchange={(e) => {this.getName(e.target.value)}}></input>
             <input type="text-area" value={this.state.value} onChange={(e) => {this.getThought}}></input>
-            <button className="button" onClick={() => this.props.post()}></button>
+            <button className="button" onClick={ () => {this.props.post({ ...this.state } ) } }>Submit</button>
         </div>
     }
 }
