c11322c1bb12bc78caec1d0261728da183166820
Jamrod <myfavoritenoisemaker@gmail.com>
2020-01-21 10:57:39 -0700

    Controller done

--- backend/controllers/Thought.js
+++ backend/controllers/Thought.js
@@ -8,6 +8,35 @@ router.get('/', (req, res) => {
     .then(thought => res.json(thoughts))
 })
 
+router.get('/:id', (req, res) => {
+    Thought.findOne({_id: req.params.id})
+    .then(thought => res.json(thought))
+} )
 
+router.post('/', (req, res) => {
+    Thought.create(req.body)
+    .then(() => res.redirect('/'))
+})
+
+router.put('/:id', (req, res) => {
+    Thought.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new: true}
+    ).then(() => res.redirect('/'))
+})
+
+router.put('/comment/:id', (req, res) => {
+    Thought.findById(req.params.id)
+    .then(thought => {
+        thought.comments.push(req.body.comment)
+        thought.save()
+    }).then((thought) => res.redirect('/' + thought._id)) 
+})
+
+router.delete('/:id', (req, res) => {
+    Thought.findByIdAndDelete(this.params.id)
+    .then(() => res.redirect('/'))
+})
 
 module.exports = router
