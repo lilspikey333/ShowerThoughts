5217f274cc54254f1acb593ac55bd1b8e516258c
Jen Valladares <lilspikey333@yahoo.com>
2020-01-21 11:54:51 -0600

    basic components made

---
+++ frontend/src/components/ThoughtBox/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ThoughtBox() {
+  return (
+    <div className="thought-box">
+      <h4>This is a Thought Box</h4>
+    </div>
+  );
+}
+
+export default ThoughtBox;
---
+++ frontend/src/components/Thoughts/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ThoughtBox from "../ThoughtBox";
+
+function Thoughts() {
+  return (
+    <div className="thoughts">
+      <ThoughtBox />
+    </div>
+  );
+}
+export default Thoughts
