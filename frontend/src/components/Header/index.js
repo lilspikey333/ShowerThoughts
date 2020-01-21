79e1398686877c3435719c380f06092abf3b3f03
Jen Valladares <lilspikey333@yahoo.com>
2020-01-21 15:24:04 -0600

    header

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -2,8 +2,9 @@ import React from "react";
 
 function Header() {
   return (
-    <header>
+    <header className="header">
       <h1>This is the Header</h1>
+      <button className="new-button">New Thought</button>
     </header>
   );
 }
--- frontend/src/components/Post/index.js
+++ frontend/src/components/Post/index.js
@@ -11,24 +11,44 @@ class Post extends Component {
 
   getName = str => {
     this.setState(prevState => ({ name: str }));
+  };
+
+  getThought = str => {
+    this.setState(prevState => ({ thought: str }));
+  };
+
+  // postThought = () => {
+  //     this.props.post({ ...this.state })
+  // }
+
+  render() {
+    <div className="flex-container-column new-post">
+      <input
+        ref={input => (this.input = input)}
+        type="text"
+        id="name-input"
+        value={this.state.name}
+        onchange={e => {
+          this.getName(e.target.value);
+        }}
+      ></input>
+      <input
+        type="text-area"
+        value={this.state.value}
+        onChange={e => {
+          this.getThought;
+        }}
+      ></input>
+      <button
+        className="button"
+        onClick={() => {
+          this.props.post({ ...this.state });
+        }}
+      >
+        Submit
+      </button>
+    </div>;
   }
-
-
-    getThought = (str) => {
-        this.setState( prevState => ({thought: str}))
-    }
-
-    // postThought = () => {
-    //     this.props.post({ ...this.state })
-    // }
-
-    render() {
-        <div className="flex-container-column new-post">
-            <input ref={(input) => this.input = input} type="text" id="name-input" value={this.state.name} onchange={(e) => {this.getName(e.target.value)}}></input>
-            <input type="text-area" value={this.state.value} onChange={(e) => {this.getThought}}></input>
-            <button className="button" onClick={ () => {this.props.post({ ...this.state } ) } }>Submit</button>
-        </div>
-    }
 }
 
-export default Post
+export default Post;
