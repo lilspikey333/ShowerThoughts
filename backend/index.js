4f72f9ce7ec6ba581870bf09b0093f24761769fe
Jen Valladares <lilspikey333@yahoo.com>
2020-01-21 10:40:23 -0600

    index.js

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const parser = require("body-parser");
+const cors = require("cors");
+
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+
+app.use(cors());
+
+app.listen(4000, () => console.log("App is running on 4000!"));
